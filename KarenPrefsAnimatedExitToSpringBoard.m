0b3602da8e9420985dd98468d24da7bc8513a368
Karen Tsai <karen@angelxwind.net>
2018-07-14 00:35:30 -0400

    Fixed an issue where KarenPrefsAnimatedExitToSpringBoard would not always work properly on iOS 11

--- KarenPrefsAnimatedExitToSpringBoard.m
+++ KarenPrefsAnimatedExitToSpringBoard.m
@@ -1,4 +1,5 @@
 #import "KarenPrefsAnimatedExitToSpringBoard.h"
+#import <version.h>
 
 @implementation UIApplication (KarenPrefsAnimatedExitToSpringBoard)
 -(void) karenPrefsAnimatedExit {
@@ -9,7 +10,10 @@
 	if ([self respondsToSelector:@selector(suspend)]) {
 		if (multitaskingSupported) {
 			[self beginBackgroundTaskWithExpirationHandler:^{}];
-			[self performSelector:@selector(exit) withObject:nil afterDelay:0.4];
+			// iOS 11 changed something and now delay values above 0 no longer work reliably (sometimes it will work, other times it will not)
+			// Setting the delay value to 0 makes it look… ugly, to say the least (the app will flash to black before animating), but it's the best we can do until I figure out a better solution.
+			// Note that setting a delay of 0 does not equate to instantaneous call, it still waits on the thread properly so the animation /does/ still work
+			[self performSelector:@selector(exit) withObject:nil afterDelay:((kCFCoreFoundationVersionNumber < kCFCoreFoundationVersionNumber_iOS_11_0) ? 0.4 : 0)];
 		}
 		[self suspend];
 	} else {
