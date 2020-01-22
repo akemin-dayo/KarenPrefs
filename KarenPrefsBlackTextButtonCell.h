587c204975b55c8a50cb54d269c4fbe145f0c204
Hiraku <dev@hiraku.tw>
2020-01-22 23:30:53 +0800

    iOS 13 dark mode

--- KarenPrefsBlackTextButtonCell.h
+++ KarenPrefsBlackTextButtonCell.h
@@ -1,4 +1,12 @@
 #import "KarenPrefsCustomTextColorButtonCell.h"
 
+#ifndef kCFCoreFoundationVersionNumber_iOS_13_0
+#define kCFCoreFoundationVersionNumber_iOS_13_0 1665.15
+#endif
+
+@interface UITraitCollection (Private)
+@property(nonatomic, readonly) int userInterfaceStyle;
+@end
+
 @interface KarenPrefsBlackTextButtonCell : KarenPrefsCustomTextColorButtonCell
 @end
